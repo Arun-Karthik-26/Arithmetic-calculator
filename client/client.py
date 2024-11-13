2ec8c0bbb8548740c29c5440be596c2571a414ca
Arun Karthik S <127036707+Arun-Karthik-26@users.noreply.github.com>
2024-11-13 18:47:24 +0530

    hosting

--- client/client.py
+++ client/client.py
@@ -53,4 +53,5 @@ def calculate():
     return render_template('index.html', response=response)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
